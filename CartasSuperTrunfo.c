d0add7823e66c74a12580640246bebeaaf8bed39
André Araujo <af_andrefelipe@hotmail.com>
2025-05-09 19:27:21 +0000

    Meu primeiro commit (criei o printf)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,7 @@
 //Teste larissa
 
 int main() {
+    printf("Desafio Cartas Super Trunfo.....\n");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -18,5 +19,4 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
 }
